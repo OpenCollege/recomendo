ce3cbaba93cc3ac69b45b96697424fb8229b104b
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-12-11 17:19:14 -0300

    Update cors_controller.rb

--- app/controllers/cors_controller.rb
+++ app/controllers/cors_controller.rb
@@ -1,7 +1,7 @@
 class CorsController < ApplicationController
   def get_title
     require 'open-uri'
-    title = Nokogiri::HTML(open(params[:url])).css('title').text
+    title = Nokogiri::HTML(open(params[:url],"User-Agent" => "Ruby/#{RUBY_VERSION}")).css('title').text
     render json: { title: title }
   end
 end
