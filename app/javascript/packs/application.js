3fa816a07050713fa3ca5a0045ace211c9d610d5
Lucas Kuhn <lucas@nordweg.com>
2020-03-28 20:20:49 -0300

    adds prompt to homescreen to custom js folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// Custom files
+require("custom/add_to_homescreen")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
