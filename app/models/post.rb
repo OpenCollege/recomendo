cdeb980f60536592a225aacd8397df4e2d0b962d
Lucas Kuhn <lucas@nordweg.com>
2020-03-29 21:10:22 -0300

    adds act-as-taggable

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,11 @@
 class Post < ApplicationRecord
+  acts_as_taggable
   belongs_to :category, optional: true
-
-  has_and_belongs_to_many :tags
 
   def self.filter(params)
     posts = Post.all.order(created_at: :desc)
     posts = posts.where(category_id: params[:category_id]) if params[:category_id]
-    posts = posts.where(tag: params[:tag]) if params[:tag]
+    posts = posts.tagged_with(params[:tags]) if params[:tags]
     posts
   end
 end
