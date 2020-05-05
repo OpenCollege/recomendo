ac8a1c8abc4522e74487eb6ef7ebd7cc2ea79a73
Lucas Kuhn <lucas@nordweg.com>
2020-05-05 11:15:42 -0300

    moves css back to require

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,10 +12,18 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// Custom CSS files
+require("css/site")
+
 // Bootstrap JS
 import JQuery from 'jquery';
 window.$ = JQuery;
 import("bootstrap");
+
+// // Custom JS files
+import("js/add_to_homescreen")
+import("js/fontawesome")
+import("js/tagsinput")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -23,14 +31,6 @@ import("bootstrap");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-// Custom CSS files
-import("../stylesheets/site")
-
-// // Custom JS files
-import("js/add_to_homescreen")
-import("js/fontawesome")
-import("js/tagsinput")
 
 
 if (navigator.serviceWorker) {
