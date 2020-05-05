0952024172b8f54585f175a4da0e71b47a701ca0
Lucas Kuhn <lucas@nordweg.com>
2020-05-05 10:34:55 -0300

    moves around

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ require("channels")
 // Bootstrap JS
 import JQuery from 'jquery';
 window.$ = JQuery;
-import("bootstrap");
+require("bootstrap");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,6 +24,15 @@ import("bootstrap");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+// Custom CSS files
+require("css/site")
+
+// // Custom JS files
+import("js/add_to_homescreen")
+import("js/tagsinput")
+import("js/fontawesome")
+
+
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js', { scope: './' })
     .then(function(reg) {
@@ -31,11 +40,3 @@ if (navigator.serviceWorker) {
       console.log(reg);
     });
 }
-
-// Custom CSS files
-import("css/bootstrap_custom")
-
-// // Custom JS files
-import("js/add_to_homescreen")
-import("js/tagsinput")
-import("js/fontawesome")
